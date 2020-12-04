b9d06372e73f7ee480eac668ca36fbd29f50ab7c
Filip Znaor <fznaor@gmail.com>
2020-12-04 19:06:46 +0100

    Fixed conflicts.

--- project.c
+++ project.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-<<<<<<< HEAD
-   printf("Pozdrav iz aplikacije od Tomislav Kušević 1234");
-=======
    printf("Pozdrav iz aplikacije od Tomislav Kušević");
->>>>>>> 996eb31 (Added Main to project.)
    return 0;
 }
