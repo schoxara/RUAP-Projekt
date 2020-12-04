822f8cb3d654f99a70361911d8ab2602dacc9f42
Filip Znaor <fznaor@gmail.com>
2020-12-04 18:56:12 +0100

    Cherry picked to commit before change message.

--- project.c
+++ project.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
+<<<<<<< HEAD
    printf("Pozdrav iz aplikacije od Tomislav Kušević 1234");
+=======
+   printf("Pozdrav iz aplikacije od Tomislav Kušević");
+>>>>>>> 996eb31 (Added Main to project.)
    return 0;
 }
