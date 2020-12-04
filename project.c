77341404c32529b5dba2414c8e8510d5f00dc6f7
Filip Znaor <fznaor@gmail.com>
2020-12-04 18:58:25 +0100

    Changed message in main.

--- project.c
+++ project.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Pozdrav iz aplikacije od Tomislav Kušević");
+   printf("Pozdrav iz aplikacije od Tomislav Kušević 1234");
    return 0;
 }
